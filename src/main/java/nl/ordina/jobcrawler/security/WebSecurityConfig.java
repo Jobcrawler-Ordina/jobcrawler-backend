d732892624927b286b9231facd9d3ff261ffce31
tkoen93 <timokoen93@gmail.com>
2020-08-19 11:02:17 +0200

    Allow all get requests to /vacancies and /skills

--- src/main/java/nl/ordina/jobcrawler/security/WebSecurityConfig.java
+++ src/main/java/nl/ordina/jobcrawler/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import nl.ordina.jobcrawler.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -58,6 +59,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/vacancies/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/skills/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
