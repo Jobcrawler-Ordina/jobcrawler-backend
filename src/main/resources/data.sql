b176296e5d481d9a8ec0f75daf992daf1db0758a
tkoen93 <timokoen93@gmail.com>
2020-08-19 11:02:58 +0200

    Add some skills upon initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO roles(name) VALUES('ROLE_USER') ON CONFLICT DO NOTHING;
 INSERT INTO roles(name) VALUES('ROLE_ADMIN') ON CONFLICT DO NOTHING;
+
+INSERT INTO skill(id, name) VALUES(uuid_in((md5((random())::text))::cstring), 'Java') ON CONFLICT DO NOTHING;
+INSERT INTO skill(id, name) VALUES(uuid_in((md5((random())::text))::cstring), 'Angular') ON CONFLICT DO NOTHING;
+INSERT INTO skill(id, name) VALUES(uuid_in((md5((random())::text))::cstring), 'Python') ON CONFLICT DO NOTHING;
+INSERT INTO skill(id, name) VALUES(uuid_in((md5((random())::text))::cstring), 'Azure') ON CONFLICT DO NOTHING;
+INSERT INTO skill(id, name) VALUES(uuid_in((md5((random())::text))::cstring), 'AWS') ON CONFLICT DO NOTHING;
