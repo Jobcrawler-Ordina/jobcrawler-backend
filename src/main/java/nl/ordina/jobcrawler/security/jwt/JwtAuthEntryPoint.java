c16df2c526a403242fdeff8cff1e3d8db5db989a
tkoen93 <timokoen93@gmail.com>
2020-08-24 06:09:18 +0200

    Change response status

--- src/main/java/nl/ordina/jobcrawler/security/jwt/JwtAuthEntryPoint.java
+++ src/main/java/nl/ordina/jobcrawler/security/jwt/JwtAuthEntryPoint.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,9 +16,9 @@ public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
-                         AuthenticationException e) throws IOException, ServletException {
+                         AuthenticationException e) throws IOException {
 
         log.error("Unauthorized error. Message - {}", e.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error -> Unauthorized");
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Error -> Unauthorized");
     }
 }
