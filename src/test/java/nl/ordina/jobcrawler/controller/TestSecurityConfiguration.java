3cec4c4ad71a125a89757873b0750429ae602ce7
tkoen93 <timokoen93@gmail.com>
2020-09-01 11:52:20 +0200

    Add comment to custom config file

--- src/test/java/nl/ordina/jobcrawler/controller/TestSecurityConfiguration.java
+++ src/test/java/nl/ordina/jobcrawler/controller/TestSecurityConfiguration.java
@@ -2,9 +2,13 @@ package nl.ordina.jobcrawler.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * This class disables the httpSecurity and allows all requests without additional permission.
+ * To be used in test environment to easily test controllers.
+ */
 
 @Configuration
 @EnableWebSecurity
