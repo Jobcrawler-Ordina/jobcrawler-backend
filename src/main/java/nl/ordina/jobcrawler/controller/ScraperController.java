a74e9c4abbac5654e296bb7af29a75328cc6e2aa
tkoen93 <timokoen93@gmail.com>
2020-09-07 13:36:22 +0200

    Add return value to /scraper endpoint

--- src/main/java/nl/ordina/jobcrawler/controller/ScraperController.java
+++ src/main/java/nl/ordina/jobcrawler/controller/ScraperController.java
@@ -2,11 +2,15 @@ package nl.ordina.jobcrawler.controller;
 
 import nl.ordina.jobcrawler.service.ScraperService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -25,13 +29,15 @@ public class ScraperController {
      */
     @PutMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public void scrape() {
+    public ResponseEntity<Object> scrape() {
         /* made in a new thread so that the sender of the request does not have to wait for a response until the
          * scraping is finished.
          */
 
         Thread newThread = new Thread(scraperService::scrape);
         newThread.start();
+
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("success", true));
 
     }
 }
