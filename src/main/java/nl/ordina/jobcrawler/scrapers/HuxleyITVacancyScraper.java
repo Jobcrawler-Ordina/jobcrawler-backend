350b01273f565fdecdf65adb27f32905b5c344cb
arti00 <arti.flinkerbusch@ordina.nl>
2020-10-01 10:19:12 +0200

    Corrected 1 line for Date formatting in the HuxleyITVacancyScraper (gave an error) and made it that locations can be retrieved without authentication being necessary.

--- src/main/java/nl/ordina/jobcrawler/scrapers/HuxleyITVacancyScraper.java
+++ src/main/java/nl/ordina/jobcrawler/scrapers/HuxleyITVacancyScraper.java
@@ -32,7 +32,8 @@ public class HuxleyITVacancyScraper extends VacancyScraper {
 
     private final Pattern ymdPattern = Pattern
             .compile("^[0-9]{4}-(1[0-2]|0[1-9])-(3[01]|[12][0-9]|0[1-9])T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{6}[0-9]?Z$");
-    private final DateTimeFormatter ymdFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS'Z'");
+    private final DateTimeFormatter ymdFormatter = DateTimeFormatter.ofPattern("[yyyy-MM-dd'T'HH:mm:ss.SSSSSSS'Z']"
+                                                                             + "[yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z']");
 
     private static final String VACANCY_URL_PREFIX = "https://www.huxley.com/nl-nl/job/kyc/";
 
