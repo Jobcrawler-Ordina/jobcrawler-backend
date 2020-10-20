e0d5aa707f5c25ed7e7274e6cb3b9f1cc5c3b2ad
toornm <toornm@gmail.com>
2020-10-20 12:29:21 +0200

    Added dates to request object

--- src/main/java/nl/ordina/jobcrawler/payload/SearchRequest.java
+++ src/main/java/nl/ordina/jobcrawler/payload/SearchRequest.java
@@ -1,22 +1,26 @@
 package nl.ordina.jobcrawler.payload;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Set;
 
 // on the server jackson gives an error that this class does not have a default constructor, which cannot exist with a final variable
 //@Value  // makes getters and makes attributes private final
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
+@Data
 public class SearchRequest {
 
-    private String location;
     private String distance;
     private String keywords;
-    private ArrayList<String> skills;
+    private Set<String> skills;
 
+    private LocalDateTime fromDate;
+    private LocalDateTime toDate;
 
 }
