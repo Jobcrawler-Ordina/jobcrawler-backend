f657f73b6f5aba15b9bf9b370797a64aa1bbed9d
toornm <toornm@gmail.com>
2020-10-20 12:30:40 +0200

    Added dates to request object

--- src/main/java/nl/ordina/jobcrawler/payload/SearchRequest.java
+++ src/main/java/nl/ordina/jobcrawler/payload/SearchRequest.java
@@ -2,15 +2,11 @@ package nl.ordina.jobcrawler.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Set;
 
-// on the server jackson gives an error that this class does not have a default constructor, which cannot exist with a final variable
-//@Value  // makes getters and makes attributes private final
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
--- src/main/java/nl/ordina/jobcrawler/service/VacancyService.java
+++ src/main/java/nl/ordina/jobcrawler/service/VacancyService.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 import static nl.ordina.jobcrawler.repo.VacancySpecifications.*;
